2cfef005789d11380768fa9a7befeca3f7a83b9e
JiHyeong23 <ekdl1120@gmail.com>
2023-08-16 23:32:00 +0900

    Feat: util 메서드 구현

--- src/main/java/MOIM/svr/utilities/UtilMethods.java
+++ src/main/java/MOIM/svr/utilities/UtilMethods.java
@@ -1,5 +1,9 @@
 package MOIM.svr.utilities;
 
+import MOIM.svr.comment.CommentRepository;
+import MOIM.svr.comment.commentDto.CommentResponseDto;
+import MOIM.svr.post.Post;
+import MOIM.svr.post.PostRepository;
 import MOIM.svr.security.JwtHelper;
 import MOIM.svr.user.User;
 import MOIM.svr.user.UserRepository;
@@ -7,16 +11,27 @@ import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Component;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.List;
 
 @Component
 @AllArgsConstructor
 public class UtilMethods {
     private UserRepository userRepository;
     private JwtHelper jwtHelper;
+    private PostRepository postRepository;
+    private CommentRepository commentRepository;
 
     public User parseTokenForUser(HttpServletRequest request) {
         String token = request.getHeader("Authorization").substring(7);
         String email = jwtHelper.getEmailFromJwtToken(token);
         return userRepository.findByEmail(email);
+    }
+
+    public Post findPost(Long postId) {
+        return postRepository.findById(postId).get();
+    }
+
+    public List<CommentResponseDto> getComments(Post post) {
+        return commentRepository.findCommentResponseDtoByPost(post);
     }
 }
