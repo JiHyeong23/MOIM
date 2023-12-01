7680e2a50ff387e2d1a0693c8a78bd5a675a9f00
JiHyeong23 <ekdl1120@gmail.com>
2023-12-01 21:19:46 +0900

    Fix : yml

--- src/main/java/MOIM/svr/schedule/Schedule.java
+++ src/main/java/MOIM/svr/schedule/Schedule.java
@@ -4,16 +4,14 @@ import MOIM.svr.post.enums.Status;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.time.LocalDateTime;
 import java.time.Month;
 
 @Entity
 @Getter
 @Setter
+@Table(name = "schedules")
 public class Schedule {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
