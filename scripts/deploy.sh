21fd4ac305e92b2119cc31fd877dabc58fad75bc
JiHyeong23 <ekdl1120@gmail.com>
2023-12-06 16:05:27 +0900

    Fix : script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -9,12 +9,12 @@ JAR_PATH=$REPOSITORY/build/libs/$JAR_NAME
 
 CURRENT_PID=$(pgrep -f $APP_NAME)
 
-if [ -z $CURRENT_PID ]
+if [ -n "$CURRENT_PID" ]
 then
   echo "> 종료할 애플리케이션이 없습니다."
 else
-  echo "> kill -9 $CURRENT_PID"
-  kill -15 $CURRENT_PID
+  echo "> kill -15 $CURRENT_PID"
+  kill -15 "$CURRENT_PID"
   sleep 5
 fi
 
