9d2958fd31fb7e9b8119e65fca49544a9a44e216
JiHyeong23 <ekdl1120@gmail.com>
2023-12-11 21:59:38 +0900

    Feat : Log

---
+++ src/main/java/MOIM/svr/log/RequsetWrapper.java
@@ -0,0 +1,52 @@
+package MOIM.svr.log;
+
+import org.springframework.util.StreamUtils;
+
+import javax.servlet.ReadListener;
+import javax.servlet.ServletInputStream;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletRequestWrapper;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+public class RequsetWrapper extends HttpServletRequestWrapper {
+
+    private byte[] cachedInputStream;
+    public RequsetWrapper(HttpServletRequest request) throws IOException {
+        super(request);
+        InputStream requestInputStream = request.getInputStream();
+        this.cachedInputStream = StreamUtils.copyToByteArray(requestInputStream);
+    }
+
+    @Override
+    public ServletInputStream getInputStream() {
+        return new ServletInputStream() {
+            private InputStream cachedBodyInputStream = new ByteArrayInputStream(cachedInputStream);
+            @Override
+            public boolean isFinished() {
+                try {
+                    return cachedBodyInputStream.available() == 0;
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                return false;
+            }
+
+            @Override
+            public boolean isReady() {
+                return false;
+            }
+
+            @Override
+            public void setReadListener(ReadListener listener) {
+                throw new UnsupportedOperationException();
+            }
+
+            @Override
+            public int read() throws IOException {
+                return cachedBodyInputStream.read();
+            }
+        };
+    }
+}
